23dac58960103a07a8c5383eebd8fccfea7cba73
vshanbha <>
2024-04-22 20:57:44 +0200

    more languages added. Prompts made generic. Adding new languages now is limited by capabilities of underlying LLM

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,49 +13,31 @@ eng_form = st.form("eng_form")
 # llm = ChatOllama(model="mistral",temperature=0.2)
 llm = None
 
-def translate_DE(text):
+def translate(text, language):
     # Prompt
-    prompt_string = """You are any language to German translator.
-                        Autodetect the language of {{ .Prompt }} and respond strictly with translation in German.
+    prompt_string = """You are any language to {language} translator.
+                        Autodetect the language of {{ .Prompt }} and respond strictly with translation in {language}.
                         Provide multiple translations where possible with relevant commentary.
-                        For German translation provide both formal and informal versions where possible.
+                        Where possible provide both formal and informal versions.
                         Limit the translation count to a maximum of 3"""
     system_prompt = SystemMessagePromptTemplate.from_template(prompt_string)
-
+    sample_h_prompt0 = HumanMessagePromptTemplate.from_template('Translate to German language')
     sample_h_prompt1 = HumanMessagePromptTemplate.from_template('How is it going ?')
     sample_a_prompt1 = AIMessagePromptTemplate.from_template('German Informal : Wie gehts ?\n\nGerman formal : Wie geht es Ihnen ?')
     sample_h_prompt2 = HumanMessagePromptTemplate.from_template('My name is John Doe')
-    sample_a_prompt3 = AIMessagePromptTemplate.from_template('German Informal : Ich bin John Doe\n\nGerman alternative : Ich Heiße John Doe')
-    human_prompt = HumanMessagePromptTemplate.from_template('{text}')
-    chat_prompt = ChatPromptTemplate.from_messages([system_prompt, 
-                                                    sample_h_prompt1, sample_a_prompt1, 
-                                                    sample_h_prompt2, sample_a_prompt3, 
-                                                    human_prompt])
-    request = chat_prompt.format_prompt(text = text).to_messages()
-    # Run LLM model
-    response = llm.invoke(request)
-    # Print results
-    print(response)
-    return response.content
-
-def translate_EN(text):
-    # Prompt
-    prompt_string = """You are any language to English translator.
-                        Autodetect the language of {{ .Prompt }} and Respond strictly with translation in English.
-                        Provide multiple translations where possible with relevant commentary.
-                        Limit the translation count to a maximum of 3"""
-    system_prompt = SystemMessagePromptTemplate.from_template(prompt_string)
-
-    sample_h_prompt1 = HumanMessagePromptTemplate.from_template('woher kommen Sie?')
-    sample_a_prompt1 = AIMessagePromptTemplate.from_template('English : Where are you from ?\n\nEnglish alternative : Where do you come from ?')
-    sample_h_prompt2 = HumanMessagePromptTemplate.from_template('wie heißen Sie')
-    sample_a_prompt3 = AIMessagePromptTemplate.from_template('English : What is your name')
-    human_prompt = HumanMessagePromptTemplate.from_template('{text}')
-    chat_prompt = ChatPromptTemplate.from_messages([system_prompt, 
-                                                    sample_h_prompt1, sample_a_prompt1, 
-                                                    sample_h_prompt2, sample_a_prompt3, 
-                                                    human_prompt])
-    request = chat_prompt.format_prompt(text = text).to_messages()
+    sample_a_prompt2 = AIMessagePromptTemplate.from_template('German Informal : Ich bin John Doe\n\nGerman alternative : Ich Heiße John Doe')
+    sample_h_prompt3 = HumanMessagePromptTemplate.from_template('Translate to English language')
+    sample_h_prompt4 = HumanMessagePromptTemplate.from_template('woher kommen Sie?')
+    sample_a_prompt4 = AIMessagePromptTemplate.from_template('English : Where are you from ?\n\nEnglish alternative : Where do you come from ?')
+    sample_h_prompt5 = HumanMessagePromptTemplate.from_template('wie heißen Sie')
+    sample_a_prompt5 = AIMessagePromptTemplate.from_template('English : What is your name')
+    human_prompt0 = HumanMessagePromptTemplate.from_template('Translate to {language} language' )
+    human_prompt1 = HumanMessagePromptTemplate.from_template('{text}' )
+    chat_prompt = ChatPromptTemplate.from_messages([system_prompt, sample_h_prompt0,
+                                                    sample_h_prompt1, sample_a_prompt1, sample_h_prompt2, sample_a_prompt2, 
+                                                    sample_h_prompt3, sample_h_prompt4, sample_a_prompt4, sample_h_prompt5, 
+                                                    sample_a_prompt5, human_prompt0, human_prompt1])
+    request = chat_prompt.format_prompt(text = text, language = language).to_messages()
     # Run LLM model
     response = llm.invoke(request)
     # Print results
@@ -65,7 +47,9 @@ def translate_EN(text):
 with eng_form:
     selected_lang = eng_form.selectbox(
         "Select Target Language",
-        ("German","English")
+        ("German","English","Spanish","French", "Dutch",
+         "Afrikaans","Portuguese", "Danish", "Swedish", "Greek",
+         "Hindi","Marathi","Telgu", "Tamil", "Kannada","Gujrati",)
     )
     in_col, out_col = eng_form.columns(2)
     topic_text = in_col.text_area("Enter Text",placeholder="Enter Text:", height=300)
@@ -77,8 +61,5 @@ with eng_form:
     else :
         llm = ChatOpenAI( openai_api_key=openai_api_key, temperature=0.2, max_tokens=300)
         if submitted:
-            if selected_lang == "German":
-                output = translate_DE(topic_text)
-            elif selected_lang == "English":
-                output = translate_EN(topic_text)
+            output = translate(topic_text, selected_lang)
             out_col.text_area("Output language {} ".format(selected_lang), value=output, height=300)
