33b4da8db419e7b61c9d914159c2f7792e86af76
vshanbha <>
2024-05-05 19:00:06 +0200

    prompt modified to use JSON output parser

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from langchain_openai import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate
 from langchain_community.chat_models import ChatOllama
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain_core.output_parsers import JsonOutputParser
+
 
 st.title("Text Translator App")
 if "API_KEY" in st.secrets:
@@ -13,36 +16,33 @@ eng_form = st.form("eng_form")
 # llm = ChatOllama(model="mistral",temperature=0.2)
 llm = None
 
+class Translation(BaseModel):
+    language: str = Field(description="Translation to this language")
+    origin_language: str = Field(description="Autodected langauge of the {{.Prompt}}")
+    formal: str = Field(description="Formal version of translated output of the {{.Prompt}}")
+    informal: str = Field(description="Informal version of translated output of the {{.Prompt}}")
+    commentary: str = Field(description="Special commentary about the translation")
+    error: str = Field(description="Error message if there was an error in translation")
+
 def translate(text, language):
+    # Parser
+    parser = JsonOutputParser(pydantic_object=Translation)
     # Prompt
     prompt_string = """You are any language to {language} translator.
-                        Autodetect the language of {{ .Prompt }} and respond strictly with translation in {language}.
-                        Provide multiple translations where possible with relevant commentary.
+                        Autodetect the language of {{ .Prompt }} as origin_language and respond strictly with translation in {language}.
+                        Provide multiple translations where possible with relevant commentary in the origin_language.
                         Where possible provide both formal and informal versions.
-                        Limit the translation count to a maximum of 3"""
+                        """
     system_prompt = SystemMessagePromptTemplate.from_template(prompt_string)
-    sample_h_prompt0 = HumanMessagePromptTemplate.from_template('Translate to German language')
-    sample_h_prompt1 = HumanMessagePromptTemplate.from_template('How is it going ?')
-    sample_a_prompt1 = AIMessagePromptTemplate.from_template('German Informal : Wie gehts ?\n\nGerman formal : Wie geht es Ihnen ?')
-    sample_h_prompt2 = HumanMessagePromptTemplate.from_template('My name is John Doe')
-    sample_a_prompt2 = AIMessagePromptTemplate.from_template('German Informal : Ich bin John Doe\n\nGerman alternative : Ich Heiße John Doe')
-    sample_h_prompt3 = HumanMessagePromptTemplate.from_template('Translate to English language')
-    sample_h_prompt4 = HumanMessagePromptTemplate.from_template('woher kommen Sie?')
-    sample_a_prompt4 = AIMessagePromptTemplate.from_template('English : Where are you from ?\n\nEnglish alternative : Where do you come from ?')
-    sample_h_prompt5 = HumanMessagePromptTemplate.from_template('wie heißen Sie')
-    sample_a_prompt5 = AIMessagePromptTemplate.from_template('English : What is your name')
     human_prompt0 = HumanMessagePromptTemplate.from_template('Translate to {language} language' )
-    human_prompt1 = HumanMessagePromptTemplate.from_template('{text}' )
-    chat_prompt = ChatPromptTemplate.from_messages([system_prompt, sample_h_prompt0,
-                                                    sample_h_prompt1, sample_a_prompt1, sample_h_prompt2, sample_a_prompt2, 
-                                                    sample_h_prompt3, sample_h_prompt4, sample_a_prompt4, sample_h_prompt5, 
-                                                    sample_a_prompt5, human_prompt0, human_prompt1])
-    request = chat_prompt.format_prompt(text = text, language = language).to_messages()
+    human_prompt1 = HumanMessagePromptTemplate.from_template('{text}\n{format_instructions}' )
+    chat_prompt = ChatPromptTemplate.from_messages([system_prompt, human_prompt0, human_prompt1])
+    request = chat_prompt.format_prompt(text = text, language = language,
+                                        format_instructions = parser.get_format_instructions()).to_messages()
     # Run LLM model
     response = llm.invoke(request)
     # Print results
-    print(response)
-    return response.content
+    return parser.parse(response.content)
 
 with eng_form:
     selected_lang = eng_form.selectbox(
@@ -51,7 +51,7 @@ with eng_form:
          "Afrikaans","Portuguese", "Danish", "Swedish", "Greek",
          "Hindi","Marathi","Telgu", "Tamil", "Kannada","Gujrati",)
     )
-    in_col, out_col = eng_form.columns(2)
+    in_col, out_meta = eng_form.columns(2)
     topic_text = in_col.text_area("Enter Text",placeholder="Enter Text:", height=300)
     submitted = eng_form.form_submit_button("Translate")
     output = ''
@@ -62,4 +62,7 @@ with eng_form:
         llm = ChatOpenAI( openai_api_key=openai_api_key, temperature=0.2, max_tokens=300)
         if submitted:
             output = translate(topic_text, selected_lang)
-            out_col.text_area("Output language {} ".format(selected_lang), value=output, height=300)
+            out = "Formal : {} \n\nInformal : {}\n\nNote: {}".format(output['formal'], output['informal'], output['commentary'])
+
+            out_meta.text_area("Detected language {}".format(output['origin_language'])
+                                   , value=out, height=300)
